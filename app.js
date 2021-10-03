652d0b28dd1c971e0a2210135dc94d091a8b2d48
Reihaneh Namdari <r3ihan3h@yahoo.com>
2021-10-03 21:40:49 +0330

    photo problem

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use(express.static('public'));
+app.use(express.static('files'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
